1b5226c193871bb03a2b083ebf5efd6358b0217c
xianlj <xianlj@hyron-js.com>
2016-09-27 18:08:32 +0800

    insertion sorted

--- Chapter_2/insert_sort.py
+++ Chapter_2/insert_sort.py
@@ -8,6 +8,21 @@ def insertion_sort(A):
 		A[i+1] = key
 
 
+def insertion_sorted(A):
+	sorted = []
+	for i in range(A)
+
+	for j in range(1, len(A)):
+		key = A[j]
+		i = j - 1
+		while i >= 0 and A[i] > key:
+			A[i+1] = A[i]
+			i -= 1
+		A[i+1] = key
+	return A
+
+
 a = [10,9,8,7,5,6,4,2,3,1,52,12]
-insertion_sort(a)
+b = insertion_sorted(a)
 print a
+print b
